73684c973606ffc1e693a3219df8e94f1c7b19f9
mwbi <mwbislh@gmail.com>
2023-04-28 00:38:05 +0430

    gggg

--- js/app2.js
+++ js/app2.js
@@ -1,7 +1,7 @@
 //module
 import * as theme from "./_theme.js";
-import { showHistory, historyArray, historyMsg, historySection, historyTrash } from "../js/_history.js";
-import { memorySection, memoryMsg, memoryTrash } from './_memory.js'
+import { showHistory, historyArray, historyMsg, historySection, historyTrash } from "./_history.js";
+import { memorySection, memoryMsg, memoryTrash , mDrop } from './_memory.js'
 
 //Variable
 const buttonContainer = document.querySelector('.numbers');
